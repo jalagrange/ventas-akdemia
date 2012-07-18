177a2ad0267987895cdc6ac2a6565481882dbc1f
Juan Andres Lagrange <jalagrange@gmail.com>
2012-07-18 14:52:34 -0430

    Precompiling assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,8 @@ VentasAkdemia::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  config.assets.precompile += %w( dataTables.bootstrapPagination.js public.css  )
+  
 end
